79095f1c056c5fc648dbed2d00ec38a0181038af
bbrooker <latte.book@icloud.com>
2016-05-09 18:53:03 +0700

    Create RedWarHeroesCrystal.java

---
+++ net/bbrooker/minespace/Items/Crystal/WarHeroes/RedWarHeroesCrystal.java
@@ -0,0 +1,31 @@
+package net.bbrooker.minespace.Items
+
+import java.util.Random;
+import java.util.List;
+
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.bbrooker.minespace.MCSpaceInfo;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+
+public class RedWarHeroesCrystal extends Item {
+	
+	public RedWarHeroesCrystal(int id) {
+		super(id);
+		setMaxStackSize(1);
+		setCreativeTab(CreativeTabs.tabMaterials);
+		setUnlocalizedName("RedWarHeroesCrystal");
+		setTextureName("minespace:RedWarHeroesCrystal");
+	}
+	
+	@SideOnly(Side.CLIENT) //add item lore
+	public void addInformation(ItemStack itemStack, EntityPlayer entityPlayer, List list, boolean is)
+		{
+		list.add("[LEGENDARY] Red shade, Sins era Crystal");
+		}
+		
+	}
