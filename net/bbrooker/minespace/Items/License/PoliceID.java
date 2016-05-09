99de275159d4332ed6d37a0584bf32e9c70f7430
lookmaiarin <lukmai1398@gmail.com>
2016-05-09 19:13:09 +0700

    Create PoliceID.java

---
+++ net/bbrooker/minespace/Items/License/PoliceID.java
@@ -0,0 +1,31 @@
+package net.bbrooker.minespace.Items
+
+import java.util.Random;
+import java.util.List;
+
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.bbrooker.minespace.MCSpaceInfo;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+
+public class PoliceID extends Item {
+	
+	public PoliceID(int id) {
+		super(id);
+		setMaxStackSize(1);
+		setCreativeTab(CreativeTabs.tabMaterials);
+		setUnlocalizedName("PoliceID");
+		setTextureName("minespace:PoliceID");
+	}
+	
+	@SideOnly(Side.CLIENT) //add item lore
+	public void addInformation(ItemStack itemStack, EntityPlayer entityPlayer, List list, boolean is)
+		{
+		list.add("[GS] Use for exchange item"); //EDIT HERE
+		}
+		
+	}
