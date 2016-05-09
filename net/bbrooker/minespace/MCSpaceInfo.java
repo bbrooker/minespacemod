b8798309f2e5f7a1735a0772cfc2d2c5f6e48a87
bbrooker <latte.book@icloud.com>
2016-05-09 13:03:01 +0700

    Create MCSpaceInfo.java

---
+++ net/bbrooker/minespace/MCSpaceInfo.java
@@ -0,0 +1,16 @@
+package net.bbrooker.minespace;
+
+public class MCSpaceInfo {
+	
+	public static final String ID = "Minespace";
+	public static final String Name = "Minespace";
+	public static final String VERS = "0.1B";
+	public static final String DES = "This mod is only use in Minespace Server";
+    public static final String DEV = "Develop by Henry Foster (Tanawat Boonmak)";
+    public static final String URL = "http://www.facebook.com/minespacerp";
+    public static final String CLEINTPROXY = "net.bbrooker.minespace.";
+	public static final String COMMONPROXY = "net.bbrooker.minespace.";
+	
+	
+
+}
