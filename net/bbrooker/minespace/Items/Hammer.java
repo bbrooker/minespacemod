a66f09424c75bd3d8635abc7ffaca13b146c06da
bbrooker <latte.book@icloud.com>
2016-05-09 18:02:54 +0700

    Create Hammer.java

---
+++ net/bbrooker/minespace/Items/Hammer.java
@@ -0,0 +1,33 @@
+package net.bbrooker.minespace.Items
+
+import java.util.Random;
+import java.util.List;
+
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.bbrooker.minespace.MCSpaceInfo;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.EntityLivingBase;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+
+public class Hammer extends Item {
+	
+	public Hammer(int id) {
+		super(id);
+		setMaxStackSize(64);
+		setCreativeTab(CreativeTabs.tabMaterials);
+		setUnlocalizedName("Hammer");
+		setTextureName("minespace:Hammer");
+	}
+	
+	@SideOnly(Side.CLIENT) //add item lore
+	public void addInformation(ItemStack itemStack, EntityPlayer entityPlayer, List list, boolean is)
+		{
+		list.add("Tool that use to craft weapon and other things");
+		}
+		
+	}
+	
+	
